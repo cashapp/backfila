877dda4a4cb12f0c107b7ae414606cf7121bee6b
Michael Gershunovsky <mgersh@squareup.com>
2019-03-11 16:45:56 -0400

    Add empty client and protos to backfila

---
+++ client/src/main/kotlin/com/squareup/cash/backfila/client/BackfilaClient.java
@@ -0,0 +1,5 @@
+package com.squareup.cash.backfila.client;
+
+public interface BackfilaClient {
+  void configureService(/*TODO*/);
+}
---
+++ client/src/main/kotlin/com/squareup/cash/backfila/client/RealBackfilaClient.java
@@ -0,0 +1,47 @@
+package com.squareup.cash.backfila.client;
+
+import com.squareup.moshi.Moshi;
+import com.squareup.protos.cash.backfila.service.ConfigureServiceRequest;
+import com.squareup.protos.cash.backfila.service.ConfigureServiceResponse;
+import com.squareup.protos.cash.backfila.service.ServiceType;
+import com.squareup.wire.WireJsonAdapterFactory;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.Collections;
+import okhttp3.OkHttpClient;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
+import static com.google.common.base.Preconditions.checkState;
+
+public class RealBackfilaClient implements BackfilaClient {
+  private final BackfilaApi backfilaApi;
+
+  public RealBackfilaClient(String host, OkHttpClient okHttpClient) {
+    Moshi moshi = new Moshi.Builder()
+        .add(new WireJsonAdapterFactory())
+        .build();
+    Retrofit retrofit = new Retrofit.Builder()
+        .baseUrl(host)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .client(okHttpClient)
+        .build();
+    this.backfilaApi = retrofit.create(BackfilaApi.class);
+  }
+
+  @Override public void configureService() {
+    ConfigureServiceRequest request = new ConfigureServiceRequest.Builder()
+        .service_type(ServiceType.CLOUD)
+        .backfills(Collections.emptyList())
+        .build();
+
+      try {
+        Response<ConfigureServiceResponse> response =
+            backfilaApi.configureService(request).execute();
+        checkState(response.isSuccessful());
+      } catch (IOException e) {
+        throw new UncheckedIOException(e);
+      }
+  }
+}
