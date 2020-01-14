2ed425bb768dd3b26eb3d9251840a705b4e9c04f
Michael Pawliszyn <mikepaw@squareup.com>
2020-01-14 14:57:48 -0500

    Bump to Misk-Web 0.1.24

--- service/web/tabs/app/src/routes/index.tsx
+++ service/web/tabs/app/src/routes/index.tsx
@@ -19,7 +19,8 @@ const routes = (
       <Route path="/app/backfills/:id" component={BackfillStatusContainer} />
       <Route path="/_admin/app/" component={HomeContainer} />
       <Route path="/_tab/app/" component={HomeContainer} />
-      <Route path="/app/ext/" component={LayoutContainer} /> {/* namespace for externally provided tabs that will still have the navbar */}
+      <Route path="/app/ext/" component={LayoutContainer} />{" "}
+      {/* namespace for externally provided tabs that will still have the navbar */}
       <Route path="/app/" component={HomeContainer} />
       {/* Do not include a Route without a path or it will display during on all tabs */}
     </Switch>
