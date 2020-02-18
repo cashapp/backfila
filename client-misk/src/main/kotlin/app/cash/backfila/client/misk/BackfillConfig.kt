3905d5437b76d8c5458d5b84cbc470b9e4ae8090
David Reed <dreed@squareup.com>
2020-02-18 16:17:51 +1100

    Add javadoc to BackfillConfig.parameters and accessor functio "parameter"

--- client-misk/src/main/kotlin/app/cash/backfila/client/misk/BackfillConfig.kt
+++ client-misk/src/main/kotlin/app/cash/backfila/client/misk/BackfillConfig.kt
@@ -3,6 +3,15 @@ package app.cash.backfila.client.misk
 import okio.ByteString
 
 data class BackfillConfig(
+  /**
+   * Populated from class level @Parameter annotations
+   * Use parameter function to get just the value (as a String)
+   * @see com.squareup.backfila.client.Parameter
+   */
   val parameters: Map<String, ByteString>,
   val dryRun: Boolean = true
-)
+) {
+  fun parameter(name: String): String? {
+    return parameters[name]?.utf8()
+  }
+}
