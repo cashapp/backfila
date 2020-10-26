a8a08d27456e8ce699ca10696e94b60029f5e937
Michael Pawliszyn <mikepaw@squareup.com>
2020-10-26 15:57:28 -0400

    Adds an empty backend to allow no registered backfill situations.

---
+++ client-misk/src/test/kotlin/app/cash/backfila/client/misk/backend/NoBackfillsModule.kt
@@ -0,0 +1,25 @@
+package app.cash.backfila.client.misk.backend
+
+import app.cash.backfila.client.misk.BackfillModule
+import app.cash.backfila.client.misk.client.BackfilaClientConfig
+import app.cash.backfila.client.misk.embedded.EmbeddedBackfilaModule
+import misk.MiskTestingServiceModule
+import misk.environment.DeploymentModule
+import misk.inject.KAbstractModule
+import misk.logging.LogCollectorModule
+
+class NoBackfillsModule : KAbstractModule() {
+  override fun configure() {
+    install(DeploymentModule.forTesting())
+    install(LogCollectorModule())
+    install(MiskTestingServiceModule())
+
+    // We install the common backfila code but not any particular backend
+    install(BackfillModule(
+        BackfilaClientConfig(
+            url = "test.url", slack_channel = "#test"
+        )
+    ))
+    install(EmbeddedBackfilaModule())
+  }
+}
