925fb126cd12da33a5ddcd855740d21b23c11482
Mike Gershunovsky <mikebuckets@gmail.com>
2022-04-07 18:38:00 -0400

    Fix maven publish plugin (#246)

--- client-base/src/test/java/app/cash/backfila/embedded/JavaToUpperCaseTestBackfill.java
+++ client-base/src/test/java/app/cash/backfila/embedded/JavaToUpperCaseTestBackfill.java
@@ -2,6 +2,7 @@ package app.cash.backfila.embedded;
 
 import app.cash.backfila.client.BackfillConfig;
 import app.cash.backfila.client.NoParameters;
+import app.cash.backfila.client.ValidateResult;
 import app.cash.backfila.client.fixedset.FixedSetBackfill;
 import app.cash.backfila.client.fixedset.FixedSetRow;
 import java.util.ArrayList;
@@ -14,15 +15,17 @@ import org.jetbrains.annotations.NotNull;
  * Testing that creating Java backfill classes works. Parameters are not currently supported.
  */
 class JavaToUpperCaseTestBackfill extends FixedSetBackfill<NoParameters> {
-  public List<String> runOrder = new ArrayList();
+  public List<String> runOrder = new ArrayList<>();
 
   @Inject
-  public JavaToUpperCaseTestBackfill(){
+  public JavaToUpperCaseTestBackfill() {
 
   }
 
-  @Override public void checkBackfillConfig(@NotNull BackfillConfig<NoParameters> backfillConfig) {
+  @NotNull @Override
+  public ValidateResult<NoParameters> checkBackfillConfig(@NotNull BackfillConfig<NoParameters> backfillConfig) {
     // No parameters to check
+    return super.checkBackfillConfig(backfillConfig);
   }
 
   @Override public void runOne(@NotNull FixedSetRow row) {
