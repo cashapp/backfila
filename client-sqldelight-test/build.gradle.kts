5b5f564fb5d8cc06852a13171cff2b4d6a03a56a
Andrew (Paradi) Alexander <adrw@squareup.com>
2025-02-11 13:49:12 -0500

    Add view logs support, bold button for start/pause, bump Kotlin/Gradle (#415)
    The latest Misk was causing failures when trying to start the
    development service so the Kotlin/Gradle upgrade was necessary to
    unblock forward development.
    
    View logs link now works and there are bolder start/pause buttons in the
    Backfill Show page.
    
    <img width="1230" alt="Screenshot 2025-01-29 at 15 00 37"
    src="https://github.com/user-attachments/assets/e3b457ce-202f-47c0-b1a4-0c52206b9f7a"
    />
    
    <img width="670" alt="Screenshot 2025-01-29 at 15 00 50"
    src="https://github.com/user-attachments/assets/782a31a6-6ef6-4f07-840f-5f51d4e603cc"
    />

--- client-sqldelight-test/build.gradle.kts
+++ client-sqldelight-test/build.gradle.kts
@@ -12,7 +12,7 @@ sqldelight {
       dialect(libs.sqldelightMysqlDialect)
       srcDirs.setFrom(listOf("src/main/sqldelight", "src/main/resources/migrations"))
       deriveSchemaFromMigrations.set(true)
-      migrationOutputDirectory.set(file("$buildDir/resources/main/migrations"))
+      migrationOutputDirectory.set(layout.buildDirectory.dir("resources/main/migrations"))
       verifyMigrations.set(true)
     }
   }
