e3e4666dd6f45e979ebb95a0947b1048718dfc3a
Jeff Hwang <jaehwang@squareup.com>
2025-06-09 17:36:22 -0400

    add turbo frame to events and partition

--- service/src/main/kotlin/app/cash/backfila/ui/components/AutoReload.kt
+++ service/src/main/kotlin/app/cash/backfila/ui/components/AutoReload.kt
@@ -2,16 +2,17 @@ package app.cash.backfila.ui.components
 
 import kotlinx.html.TagConsumer
 import kotlinx.html.div
+import misk.turbo.turbo_frame
 
-// TODO
-//   Currently this reloads the whole page every 10s
-//   We want to ideally only update within a turbo frame so the rest of the UI is stable
-//     (ie. update form for backfill show page)
-fun TagConsumer<*>.AutoReload(block: TagConsumer<*>.() -> Unit) {
+fun TagConsumer<*>.AutoReload(frameId: String = "auto-reload-frame", block: TagConsumer<*>.() -> Unit) {
   div {
     attributes["data-controller"] = "auto-reload"
-    attributes["data-auto-reload-target"] = "frame"
 
-    block()
+    turbo_frame(frameId) {
+      attributes["data-auto-reload-target"] = "frame"
+      attributes["data-turbo-action"] = "replace"
+
+      block()
+    }
   }
 }
--- service/src/main/resources/web/static/js/auto_reload_controller.js
+++ service/src/main/resources/web/static/js/auto_reload_controller.js
@@ -1,41 +1,27 @@
 import { Application, Controller } from "../cache/stimulus/3.1.0/stimulus.min.js";
 window.Stimulus = Application.start();
 
-// Hard reload
 Stimulus.register("auto-reload", class extends Controller {
+  static targets = ["frame"]
+
   connect() {
     console.log("Auto-reload connected...");
-    this.reloadInterval = setInterval(() => {
-      console.log("Reloading page...");
-      window.location.reload();
-    }, 10000); // 10000 milliseconds = 10 seconds
+    this.startReloading()
   }
 
   disconnect() {
     console.log("Clearing auto-reload interval...");
-    clearInterval(this.reloadInterval);
+    if (this.interval) clearInterval(this.interval)
+  }
+
+  startReloading() {
+    this.interval = setInterval(() => {
+      console.log("auto-reload triggered...");
+      const pathname = window.location.pathname
+      const url = new URL(pathname, window.location.origin)
+      url.searchParams.set('frame', 'true')
+      
+      Turbo.visit(url.toString(), { frame: this.frameTarget.id })
+    }, 10000)
   }
 });
-
-
-// TODO fix soft-reload
-//Stimulus.register("auto-reload", class extends Controller {
-//  static targets = ["frame"];
-//
-//  connect() {
-//    console.log("Auto-reload connected...");
-//    this.reloadInterval = setInterval(() => {
-//      console.log("Reloading Turbo Frame...");
-//      this.reloadFrame();
-//    }, 5000); // 5000 milliseconds = 5 seconds
-//  }
-//
-//  disconnect() {
-//    console.log("Clearing auto-reload interval...");
-//    clearInterval(this.reloadInterval);
-//  }
-//
-//  reloadFrame() {
-//    this.frameTarget.src = this.frameTarget.src;
-//  }
-//});
