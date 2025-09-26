3d862dc4cb4fecb2626a98f94b19de345f69b10b
Jeff Hwang <jaehwang@squareup.com>
2025-09-26 12:41:42 -0400

    bump pkey cursor size to 1000

---
+++ service/src/main/resources/migrations/v022__backfila.sql
@@ -0,0 +1,6 @@
+-- Increase pkey_cursor and related columns to maximum varbinary size
+ALTER TABLE run_partitions
+    MODIFY COLUMN pkey_cursor varbinary(MAX NULL DEFAULT NULL,
+    MODIFY COLUMN pkey_range_start varbinary(1000) NULL DEFAULT NULL,
+    MODIFY COLUMN pkey_range_end varbinary(1000) NULL DEFAULT NULL,
+    MODIFY COLUMN precomputing_pkey_cursor varbinary(1000) NULL DEFAULT NULL;
